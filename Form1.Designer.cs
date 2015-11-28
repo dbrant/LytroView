72792c51897f937d81fa5988eb07d2d424d53b48
Dmitry Brant <dbrant@wikimedia.org>
2015-11-28 17:24:07 -0500

    Initial working code.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace MpoViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbImage = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSave});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(144, 26);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Image = ((System.Drawing.Image)(resources.GetObject("mnuSave.Image")));
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(143, 22);
+            this.mnuSave.Text = "Save image...";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(414, 389);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Image:";
+            // 
+            // tbImage
+            // 
+            this.tbImage.Location = new System.Drawing.Point(58, 27);
+            this.tbImage.Name = "tbImage";
+            this.tbImage.Size = new System.Drawing.Size(188, 45);
+            this.tbImage.TabIndex = 10;
+            this.tbImage.Scroll += new System.EventHandler(this.tbImage_Scroll);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 475);
+            this.Controls.Add(this.tbImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace MpoViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            Functions.FixDialogFont(this);
+            this.Text = Application.ProductName;
+
+            images = new List<Image>();
+            
+            if (args.Length > 0)
+            {
+                OpenLFP(args[0]);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+            openDlg.DefaultExt = ".lfp";
+            openDlg.CheckFileExists = true;
+            openDlg.Title = "Open LFP file...";
+            openDlg.Filter = "LFP Files (*.lfp)|*.lfp|All Files (*.*)|*.*";
+            openDlg.FilterIndex = 1;
+            if (openDlg.ShowDialog() == DialogResult.Cancel) return;
+            OpenLFP(openDlg.FileName);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 0) return;
+            OpenLFP(files[0]);
+        }
+
+
+        private List<Image> images;
+        private Image stereoImage = null;
+        private string FileName = "";
+
+
+        private void OpenLFP(string fileName)
+        {
+            FileName = fileName;
+            images.Clear();
+
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                byte[] tempBytes = new byte[0x10];
+                using (var f = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    while (f.Position < f.Length)
+                    {
+                        f.Read(tempBytes, 0, tempBytes.Length);
+                        string blockName = Encoding.ASCII.GetString(tempBytes, 1, 3);
+                        UInt32 blockLength = Functions.BigEndian(BitConverter.ToUInt32(tempBytes, 12));
+
+                        if (!blockName.StartsWith("LF"))
+                        {
+                            continue;
+                        }
+                        if (blockLength == 0)
+                        {
+                            continue;
+                        }
+
+                        f.Seek(0x50, SeekOrigin.Current);
+
+                        if (blockLength > 0x100)
+                        {
+                            f.Read(tempBytes, 0, 4);
+                            f.Seek(-4, SeekOrigin.Current);
+
+                            if (tempBytes[0] == 0xff && tempBytes[1] == 0xD8 && tempBytes[2] == 0xFF)
+                            {
+                                byte[] imageBytes = new byte[blockLength];
+                                f.Read(imageBytes, 0, (int) blockLength);
+                                f.Seek(-blockLength, SeekOrigin.Current);
+                                
+                                MemoryStream stream = new MemoryStream(imageBytes, 0, (int) blockLength);
+                                images.Add(new Bitmap(stream));
+                            }
+                        }
+
+                        f.Seek(blockLength, SeekOrigin.Current);
+
+                        if (blockLength % 0x10 > 0)
+                        {
+                            f.Seek(0x10 - (blockLength % 0x10), SeekOrigin.Current);
+                        }
+                    }
+                }
+                
+                if (images.Count == 0)
+                {
+                    pictureBox.Image = null;
+                    MessageBox.Show(this, "This does not appear to be a valid LFP stacked image.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    tbImage.Maximum = images.Count - 1;
+                    tbImage_Scroll(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0) return;
+            try
+            {
+                var saveDlg = new SaveFileDialog();
+                saveDlg.DefaultExt = ".jpg";
+                saveDlg.OverwritePrompt = true;
+                saveDlg.Title = "Save file...";
+                saveDlg.Filter = "JPG Files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+                saveDlg.FilterIndex = 1;
+                saveDlg.InitialDirectory = Path.GetDirectoryName(FileName);
+                saveDlg.FileName = Path.GetFileNameWithoutExtension(FileName) + "_" + (tbImage.Value + 1).ToString() + ".jpg";
+                if (saveDlg.ShowDialog() == DialogResult.Cancel) return;
+
+                //images[cbImage.SelectedIndex].Save(saveDlg.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, Application.ProductName + "\nCopyright © 2015 by Dmitry Brant\n\nhttp://dmitrybrant.com", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void tbImage_Scroll(object sender, EventArgs e)
+        {
+            if (images.Count > tbImage.Value)
+            {
+                pictureBox.Image = images[tbImage.Value];
+            } else
+            {
+                pictureBox.Image = null;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class Functions
+    {
+
+        /// <summary>
+        /// Sets the font of a given control, and all child controls, to
+        /// the current system font, while preserving font styles.
+        /// </summary>
+        /// <param name="c0">Control whose font will be set.</param>
+        public static void FixDialogFont(Control c0)
+        {
+            Font old = c0.Font;
+            c0.Font = new Font(SystemFonts.MessageBoxFont.FontFamily.Name, old.Size, old.Style);
+            if (c0.Controls.Count > 0)
+                foreach (Control c in c0.Controls)
+                    FixDialogFont(c);
+        }
+
+        private static UInt32 conv_endian(UInt32 val)
+        {
+            UInt32 temp = (val & 0x000000FF) << 24;
+            temp |= (val & 0x0000FF00) << 8;
+            temp |= (val & 0x00FF0000) >> 8;
+            temp |= (val & 0xFF000000) >> 24;
+            return (temp);
+        }
+
+        public static UInt32 BigEndian(UInt32 val)
+        {
+            if (!BitConverter.IsLittleEndian) return val;
+            return conv_endian(val);
+        }
+    }
+
+}
